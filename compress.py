980c16927cd5e3b34b708ceb6c6f357a7c1ef0a2
luigiRP <luigirosales84@gmail.com>
2020-08-18 13:26:55 +0000

    done

--- compress.py
+++ compress.py
@@ -1,18 +1,26 @@
- # !/usr/bin/python
+# !/usr/bin/python
 # coding=utf-8
 import re
 
 symbols = {
-#   key             :   symbols[key]
+    #   key             :   symbols[key]
     "implementation":   "🤯",
-    "practicality"  :   '🤩',
-    "better"        :   '😅',
-    "than"          :   '😘',
-    "Although"      :   "🥺",
+    "practicality":   '🤩',
+    "better":   '😅',
+    "than":   '😘',
+    "Although":   "🥺",
 }
+
 
 def compress(content):
 
-    compressed_content = ''    
+    compressed_content = ''
+    content = content.replace("implementation", symbols["implementation"])
+    content = content.replace("practicality", symbols["practicality"])
+    content = content.replace("better", symbols["better"])
+    content = content.replace("than", symbols["than"])
+    content = content.replace("Although", symbols["Although"])
+
+    
 
     return content
--- decompress.py
+++ decompress.py
@@ -5,7 +5,12 @@ from compress import symbols
 
 def decompress(compressed_content):
 
-    decompressed_content = '' 
+    compressed_content = compressed_content.replace(symbols["implementation"], "implementation")
+    compressed_content = compressed_content.replace(symbols["practicality"], "practicality")
+    compressed_content = compressed_content.replace(symbols["better"], "better")
+    compressed_content = compressed_content.replace(symbols["than"], "than")
+    compressed_content = compressed_content.replace(symbols["Although"], "Although")
 
+    print(compressed_content)
     return compressed_content
     
